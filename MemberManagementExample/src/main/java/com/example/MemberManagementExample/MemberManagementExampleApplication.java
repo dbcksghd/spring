89bf21f0ff61190d3b1f69442f383ea4c045ec39
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-19 15:06:52 +0900

    회원 관리 예제

---
+++ MemberManagementExample/src/main/java/com/example/MemberManagementExample/MemberManagementExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.MemberManagementExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagementExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberManagementExampleApplication.class, args);
+    }
+
+}
