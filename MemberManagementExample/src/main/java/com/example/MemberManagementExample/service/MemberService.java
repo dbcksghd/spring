ea142bc61c3941381bfb6509e93c71ff2c212ce2
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-19 18:33:39 +0900

    멤버 리포지토리를 외부에서 바꿔주도록 변경

--- MemberManagementExample/src/main/java/com/example/MemberManagementExample/service/MemberService.java
+++ MemberManagementExample/src/main/java/com/example/MemberManagementExample/service/MemberService.java
@@ -1,29 +1,33 @@
 package com.example.MemberManagementExample.service;
 
 import com.example.MemberManagementExample.domain.Member;
-import com.example.MemberManagementExample.repository.MemoryMemberRepository;
+import com.example.MemberManagementExample.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         //중복 제거
-        memoryMemberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(member1 -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     public List<Member> findMembers() {
-        return memoryMemberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-        return memoryMemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
