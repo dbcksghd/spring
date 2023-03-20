adeaeab48d102672fc583cf1f611bd127fe0b8f4
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-20 22:05:15 +0900

    회원가입 기능 추가

--- MemberManagementExample/src/main/java/com/example/MemberManagementExample/controller/MemberController.java
+++ MemberManagementExample/src/main/java/com/example/MemberManagementExample/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.MemberManagementExample.controller;
 
+import com.example.MemberManagementExample.domain.Member;
 import com.example.MemberManagementExample.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String caeate(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 
 }
